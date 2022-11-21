9f40fdaeb2adeb952a15d432e44878313e459330
Wang Xu <rabiitlbj@hotmail.com>
2022-11-22 02:42:06 +0800

    udpate setting

--- proj_3dm/proj_3dm/settings.py
+++ proj_3dm/proj_3dm/settings.py
@@ -25,10 +25,10 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
@@ -62,14 +62,14 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-IMAGES_STORE = '/workspaces/crawler-3dm/proj_3dm/images'
+IMAGES_STORE = '/root/workfolder/crawler-3dm/proj_3dm/images'
 ITEM_PIPELINES = {
    'proj_3dm.pipelines.ImgPipeline': 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 AUTOTHROTTLE_START_DELAY = 2
 # The maximum download delay to be set in case of high latencies
